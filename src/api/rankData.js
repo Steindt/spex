5750599036856201abbfb25043aff2aee60226d9
Tair <tairstenen@gmail.com>
2023-04-05 17:57:12 +0200

    Started building table for ranking

---
+++ src/api/rankData.js
@@ -0,0 +1,22 @@
+export const rankData = [{
+    id: 0,
+    rank: 1,
+    name: "Spex 1",
+    förening: "Lundaspexarna",
+    betyg: 10,
+    dittBetyg: 9
+}, {
+    id: 1,
+    rank: 2,
+    name: "Spex 2",
+    förening: "Jesperspexet",
+    betyg: 9,
+    dittBetyg: 10
+}, {
+    id: 2,
+    rank: 3,
+    name: "Spex 3",
+    förening: "Kristianstads nation",
+    betyg: 8,
+    dittBetyg: 2
+}]
--- src/components/Ranking.js
+++ src/components/Ranking.js
@@ -1,9 +1,31 @@
+import {rankData} from '../api/rankData.js'
+import './Ranking.css'
 
 export default function Ranking() {
+    const tableData = rankData.map(spex => 
+        <tr>
+            <td>{spex.rank}</td>
+            <td>{spex.name}</td>
+            <td>{spex.förening}</td>
+            <td>{spex.betyg}</td>
+            <td>{spex.dittBetyg}</td>
+        </tr>
+    );
+
     return (
-        <div className="RankingContainer">
-            <div className="RankingColumns"></div>
-            <div className="RankingList"></div>
-        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th scope="col">Rank</th>
+                    <th scope="col">Namn</th>
+                    <th scope="col">Förening</th>
+                    <th scope="col">Betyg</th>
+                    <th scope="col">Ditt betyg</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tableData}
+            </tbody>
+        </table>
     );
 }
